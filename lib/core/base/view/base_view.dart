406ffa7a6cbf9e753fe75f2a370d6cf656385c01
batuhankrbb <batuhankarababa@gmail.com>
2021-04-04 07:59:46 +0300

    BaseView for Screens Created -S0-E2-

--- lib/core/base/view/base_view.dart
+++ lib/core/base/view/base_view.dart
@@ -14,3 +14,71 @@ import 'package:flutter_templ_mvvm/core/base/state/base_state.dart';
 */
 
 //* Kısacası ViewModel yapısına çok uygun bir base yapıyoruz. Bunun tabiiki böyle olması şart değil ama ana mantık burada Tüm sayfaları kaplayan ve Tüm sayfaların türediği bir Base View yapmak
+
+//? Örnek kullanım: Bütün sayfalarda internet olmazsa çıkan bir pop-up veya dialog olsun. Bunun için tüm sayfalarda tek tek tanımlamak yerine tüm sayfaları BaseView dan türetip BaseView içinde bunu tanımlamak süper olur.
+
+class BaseView<T> extends StatefulWidget { //Burada T yi ViewModel gibi bir abstract sınıftan türetebilirdik kısıtlamak için.
+  BaseView(
+      {Key key,
+      this.viewModel,
+      this.onModelReady,
+      this.onDispose,
+      this.onPageBuilder})
+      : super(key: key);
+
+  final T viewModel;
+  final Function(T model) onModelReady; //init
+  final VoidCallback onDispose; //Burada da T Model parametresi alan bir Function geçebilirdik.
+  final Widget Function(BuildContext context, T value) onPageBuilder;
+
+  @override
+  _BaseViewState createState() => _BaseViewState();
+}
+
+class _BaseViewState extends State<BaseView> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onModelReady != null) {
+      widget.onModelReady(widget.viewModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (widget.onDispose != null) {
+      widget.onDispose();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.onPageBuilder(context, widget.viewModel);
+  }
+}
+
+
+
+//? KULLANIM
+
+class TestView extends StatelessWidget {
+  const TestView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView(
+      viewModel: "",
+      onDispose: () {
+        //viewmodel öldürme işlemleri vb vb.
+      },
+      onModelReady: (viewModel) {
+        viewModel = "ViewModel";
+        //viewmodel tanıma, başlangıç işlemlerini yapma vb vb.
+      },
+      onPageBuilder: (context, viewModel) {
+        return Text("Burası aslında bildiğin build metodu olarak çalışacak.");
+      },
+    );
+  }
+}
