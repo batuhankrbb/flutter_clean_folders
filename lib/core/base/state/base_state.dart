01d21b9be50c5007269f33cabed2bf4a377498d3
batuhankrbb <batuhankarababa@gmail.com>
2021-04-04 07:30:27 +0300

    Base States Updated

--- lib/core/base/state/base_state.dart
+++ lib/core/base/state/base_state.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-
 //base stful
-abstract class BaseState <T extends StatefulWidget> extends State<T>{ //State yerine artık BaseState Kullanılır.
+abstract class BaseState<T extends StatefulWidget> extends State<T> {
+  //State yerine artık BaseState Kullanılır.
   //... Ekstra Özellikler ve metodlar
+  double dynamicHeight(double value) =>
+      MediaQuery.of(context).size.height * value;
+
+      double dynamicWidth(double value) =>
+      MediaQuery.of(context).size.width * value;
+
+      ThemeData get theme => Theme.of(context);
 }
 
-
 //base stless
-abstract class BaseStateless extends StatelessWidget{ //Stateless yerine artık BaseStateless Kullanılır.
+abstract class BaseStateless extends StatelessWidget {
+  //Stateless yerine artık BaseStateless Kullanılır.
   //... Ekstra özellikler ve metodlar
 }
--- lib/core/base/state/stful_base_test.dart
+++ lib/core/base/state/stful_base_test.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 
 //! BURASI AÇIKLAMA VE DENEME SAYFASIDIR BURAYI BOŞVER
 
+/*
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  //* State kısmı
+  // State kısmı
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,9 +19,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-//* Burada en başta T yi tanımlıyoruz StatefulWidget olacak diye.
-//* Daha sonra aslında aynı normal bir state gibi bunun içine T tipinden bir şey alıyoruz yani StatefulWidget tipinden bir şey alıyoruz.
-//* Bu da aslında bir state yani bildiğin biz State sınıfını tekrar yazıp bunu genişletiyoruz SOLID deki Open/Closed prensipindeki gibi.
+// Burada en başta T yi tanımlıyoruz StatefulWidget olacak diye.
+// Daha sonra aslında aynı normal bir state gibi bunun içine T tipinden bir şey alıyoruz yani StatefulWidget tipinden bir şey alıyoruz.
+// Bu da aslında bir state yani bildiğin biz State sınıfını tekrar yazıp bunu genişletiyoruz SOLID deki Open/Closed prensipindeki gibi.
 //
 abstract class BaseState<T extends StatefulWidget> extends State<T> {
   //build geliyo
@@ -54,7 +56,7 @@ class Deneme extends StatelessWidget {
 
 abstract class OzelStateless extends StatelessWidget {}
 
-//* ////////////////////////////////
+// ////////////////////////////////
 
 //abstract class BaseState extends State<StatefulWidget>{ // Bu şekilde yaparsak sadece statefulwidgetten türemiş sınıflar buraya gelebilir. Belki benim bir tane daha base sınıfım var ve onu buraya vermek istiyorum. Bunu bilemeyeceğim için generic kullanıp statefuldan extend eden sınıfları alıcam.
 
@@ -90,3 +92,4 @@ class DenemePage extends OzelBaseStatelessWidget {
     return Container();
   }
 }
+*/
